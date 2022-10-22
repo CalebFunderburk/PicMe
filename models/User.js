7b43a20bb7c49df2a63fd9f3bd7f69b3d07c0313
connork96 <connorkelly96@yahoo.com>
2022-10-22 15:33:18 -0400

    updated code for the user model

--- models/User.js
+++ models/User.js
@@ -6,15 +6,37 @@ User.init(
     {
         id: {
             type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
         },
         username: {
             type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
         },
         email: {
             type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+
         },
         password: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
         }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
 )
