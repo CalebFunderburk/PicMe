86c85f6d9f2ae70200c5a0f0ed04b31a64a0a517
Dane Cronin <danecronin@gmail.com>
2022-10-24 18:45:24 -0600

    signup.js not commited in last commit

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,32 +1,4 @@
-// Sign-up form logic
-async function signupFormHandler(event) {
-    event.preventDefault()
 
-    // HTML elements
-    const username = document.querySelector('#username-signup').value.trim()
-    const email = document.querySelector('#email-signup').value.trim()
-    const password = document.querySelector('#password-signup').value.trim()
-
-    // Store data if all fields are filled
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        console.log(response)
-
-        if (response.ok) {
-            console.log('success')
-        } else {
-            alert(response.statusText)
-        }
-    }
-}
 
 // Login logic
 async function loginFormHandler(event) {
@@ -57,4 +29,3 @@ async function loginFormHandler(event) {
 
 // Event listeners
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler)
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler)
---
+++ public/javascript/signup.js
@@ -0,0 +1,32 @@
+// Sign-up form logic
+async function signupFormHandler(event) {
+    event.preventDefault()
+
+    // HTML elements
+    const username = document.querySelector('#username-signup').value.trim()
+    const email = document.querySelector('#email-signup').value.trim()
+    const password = document.querySelector('#password-signup').value.trim()
+
+    // Store data if all fields are filled
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        console.log(response)
+
+        if (response.ok) {
+            console.log('success')
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+//Event Listeners
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler)
